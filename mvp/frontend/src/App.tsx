c9a27e1fa6cf3eab1240a0b2e8b2815f7a74b692
Milan Mohapatra <milanmohapatra.official@gmail.com>
2025-06-22 10:47:59 +0530

    chore: initialize project and set up shadcn ui

---
+++ mvp/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div className="text-blue-500">
+      <h2>Heading</h2>
+      <Button variant={"destructive"}>click</Button>
+    </div>
+  )
+}
+
+export default App
