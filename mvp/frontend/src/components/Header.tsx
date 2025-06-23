f8ae7b0bde9a84c8255b261d10e1d17db89c0ee1
Milan Mohapatra <milanmohapatra.official@gmail.com>
2025-06-23 14:16:03 +0530

    feat(ui): add navigation bar

--- mvp/frontend/src/components/Header.tsx
+++ mvp/frontend/src/components/Header.tsx
@@ -1,8 +1,56 @@
+import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react";
+import Container from "./Container";
+import LogoContainer from "./LogoContainer";
+import NavigationRoutes from "./NavigationRoutes";
+import { NavLink } from "react-router-dom";
+import ProfileContainer from "./ProfileContainer";
+import ToggleContainer from "./ToggleContainer";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const { userId } = useAuth();
 
-export default Header
+  return (
+    <header
+      className={cn("w-full border-b duration-150 transition-all ease-in-out")}
+    >
+      <Container>
+        <div className="flex items-center gap-4 w-full">
+          {/* logo section */}
+          <LogoContainer />
+
+          {/* navigation section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+
+            {userId && (
+              <NavLink
+                to={"/generate"}
+                className={({ isActive }) =>
+                  cn(
+                    "text-base text-neutral-600",
+                    isActive && "text-neutral-900 font-semibold"
+                  )
+                }
+              >
+                Take An Interview
+              </NavLink>
+            )}
+          </nav>
+
+          {/* profile section */}
+            <div className="ml-auto flex items-center gap-6">
+            {/* profile section */}
+            <ProfileContainer />
+
+            {/* mobile toggle section */}
+            <ToggleContainer />
+          </div>
+
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
